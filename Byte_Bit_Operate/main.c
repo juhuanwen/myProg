19dd1b5c6964a4ac6c4276a4b91b187fd038fe09
unknown <ju370785@163.com>
2020-03-14 14:31:30 +0800

    发现联合体中所有变量地址都是一样的，回头可以 重新看看联合体

--- Byte_Bit_Operate/main.c
+++ Byte_Bit_Operate/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+union{
+	unsigned char flag10;
+	struct{
+		unsigned b0:1;
+		unsigned b1:1;
+		unsigned b2:1;
+		unsigned b3:1;
+		unsigned b4:1;
+		unsigned b5:1;
+		unsigned b6:1;
+		unsigned b7:1;
+		}flag10bit;
+}myflag10;
+#define rec_status            myflag10.flag10 
+#define main_rec_err          myflag10.flag10bit.b0 
+#define work                  myflag10.flag10bit.b1
+#define miejun_result         myflag10.flag10bit.b2
+#define door_switch           myflag10.flag10bit.b3
+#define open_fmq              myflag10.flag10bit.b4
+#define water_need_close_door myflag10.flag10bit.b5
+#define Main_test             myflag10.flag10bit.b6          // mcq 130122 
+
+//发现bits1和bitss的地址是同一个地址
 typedef union
 {
+	unsigned char bits1;
+	unsigned char bits2;
 	struct {
 		unsigned char bit0	:1;
 		unsigned char bit1	:1;
@@ -16,6 +42,7 @@ typedef union
 
 }defbits;
 defbits mytest;
+#define rec_status1	mytest.bits1
 #define R_1	mytest.bitss.bit0
 #define R_2	mytest.bitss.bit1
 #define R_3	mytest.bitss.bit2
@@ -26,14 +53,19 @@ defbits mytest;
 #define R_8	mytest.bitss.bit7
 int main()
 {
+	rec_status1=0x11;
 	R_1=1;
 	R_2=1;
 	R_3=1;
 	R_4=1;
-	R_5=1;
+	R_5=0;
 	R_6=1;
 	R_7=1;
 	R_8=1;
+	
 	printf("mytest=%x\n",*(unsigned char*)(&mytest));
+	printf("Pbitss=%p\n",&mytest.bitss);
+	printf("bits1=%p\n",&mytest.bits1);
+	printf("bits2=%p\n",&mytest.bits2);
 
 }
